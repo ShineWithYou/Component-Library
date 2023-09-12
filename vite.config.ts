586d88fc664b304696ca6f760004b3b6a55b3ebb
SunShine <850780964@qq.com>
2023-09-12 23:49:03 +0800

    feat: ESLint for Vue3 & Typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import  esLint  from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), esLint()],
 })
