96eeec0b4d0d54b54030de00020c58ef5aebbe0a
SunShine <850780964@qq.com>
2023-12-14 00:27:58 +0800

    button unit test

--- src/components/Button/Button.test.ts
+++ src/components/Button/Button.test.ts
@@ -1,6 +1,8 @@
-import { describe, test } from 'vitest';
-import { mount } from '@vue/test-utils'
+import { describe, test, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
 import Button from './Button.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Icon from '../Icon/Icon.vue';
 
 describe('Button.vue', () => {
   test('basic button', () => {
@@ -13,5 +15,66 @@ describe('Button.vue', () => {
       }
     })
     console.log(wrapper.html());
+    expect(wrapper.classes()).toContain('zyt-button--primary');
+    // slot
+    expect(wrapper.get('button').text()).toBe('button');
+    // events
+    wrapper.get('button').trigger('click');
+    console.log(wrapper.emitted());
+    expect(wrapper.emitted()).toHaveProperty('click');
+  })
+  test('disabled', () => {
+    const wrapper = mount(Button, {
+      props: {
+        disabled: true
+      },
+      slots: {
+        default: 'desabled'
+      }
+    })
+    // attributes
+    expect(wrapper.attributes('disabled')).toBeDefined();
+    // attributes
+    expect(wrapper.find('button').element.disabled).toBeDefined();
+
+    wrapper.get('button').trigger('click');
+    expect(wrapper.emitted()).not.toHaveProperty('click');
+  })
+
+  test('icon', () => {
+    const wrapper = mount(Button, {
+      props: {
+        icon: 'arrow-up'
+      },
+      slots: {
+        default: 'icon'
+      },
+      global: {
+        stubs: ['FontAwesomeIcon']
+      }
+    })
+    console.log(wrapper.html());
+    const iconElement = wrapper.findComponent(FontAwesomeIcon);
+    expect(iconElement.exists()).toBeTruthy();
+    expect(iconElement.attributes('icon')).toBe('arrow-up');
+  })
+  
+  test('loading', () => {
+    const wrapper = mount(Button, {
+      props: {
+        loading: true
+      },
+      slots: {
+        default: 'loading'
+      },
+      global: {
+        stubs: ['Icon']
+      }
+    })
+    console.log(wrapper.html());
+    const iconElement = wrapper.findComponent(Icon);
+    expect(iconElement.exists()).toBeTruthy();
+    expect(iconElement.attributes('icon')).toBe('spinner');
+    expect(wrapper.attributes('disabled')).toBeDefined();
   })
 })
